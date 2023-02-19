4fc0e03d638f0881239a1ac450fe6cc8e812cd06
Hyungjun <ryanromaris@naver.com>
2023-02-19 13:49:06 +0900

    feat: test

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// module.exports = {
+//   exportPathMap: async function (
+//     defaultPathMap,
+//     { dev, dir, outDir, distDir, buildId }
+//   ) {
+//     return {
+//       '/': { page: '/' },
+//       '/menu/id': {
+//         page: '/menu/[id]',
+//         query: { id: 'id' },
+//       },
+//     };
+//   },
+// };
