7e28185d1994db2f84a6eca769efb4db070d46ca
Hyungjun <ryanromaris@naver.com>
2023-03-04 18:47:27 +0900

    feat(cors): add cors options

--- server/app.js
+++ server/app.js
@@ -3,12 +3,19 @@ import cors from 'cors';
 import categoryRouter from './router/category.js';
 import menuRouter from './router/menu.js';
 import { db } from './db/database.js';
+import { config } from './config.js';
 // import { initSocket } from './connection/socket.js';
+
+const corsOption = {
+  origin: config.cors.allowedOrigin,
+  optionsSuccessStatus: 200,
+};
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOption));
+
 app.use('/category', categoryRouter);
 app.use('/menu', menuRouter);
 
