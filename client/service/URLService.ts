76399ab57297698cfbbe244530c39fe9c270bdea
Hyungjun <ryanromaris@naver.com>
2023-03-04 18:37:57 +0900

    feat(host): add real host

--- client/service/URLService.ts
+++ client/service/URLService.ts
@@ -2,7 +2,8 @@ import Env from '../constant/Env';
 
 class URLService {
   private static DevelopmentHost = 'http://localhost:8080';
-  private static ProductionHost = 'http://localhost:8080';
+  private static ProductionHost =
+    'http://ec2-3-35-235-130.ap-northeast-2.compute.amazonaws.com:8080';
 
   static DefaultAPI = (...paths: (string | number)[]): string =>
     Env.isProduction
